bcd04a4520ba966e4e732c323570cb1816336cbc
Ben Haubrich <bjh885@mail.usask.ca>
2024-12-07 15:43:29 -0500

    Change name of power abstraction

---
+++ Abstractions/Hardware/PowerResetClockManagementAbstraction.hpp
@@ -0,0 +1,28 @@
+/***************************************************************************//**
+* @author   Ben Haubrich
+* @file     PowerResetClockManagementAbstraction.hpp
+* @details  Abstraction layer for power and clock management
+* @ingroup  AbstractionLayer
+*******************************************************************************/
+#ifndef __POWER_RESET_CLOCK_MANAGEMENT_HPP__
+#define __POWER_RESET_CLOCK_MANAGEMENT_HPP__
+
+//AbstractionLayer
+#include "Error.hpp"
+#include "Types.hpp"
+
+/**
+ * @class PowerResetClockManagementAbstraction
+ * @brief Abstraction layer for power, reset, and clock management
+ */
+class PowerResetClockManagementAbstraction {
+    public:
+    ///@brief constructor
+    PowerResetClockManagementAbstraction() = default;
+    ///@brief destructor
+    virtual ~PowerResetClockManagementAbstraction() = default;
+
+    virtual ErrorType init() = 0;
+}
+
+#endif // __POWER_RESET_CLOCK_MANAGEMENT_HPP__
