f0cbe94bd9a284fec04189b38b03317cf609209f
Ben Haubrich <bjh885@mail.usask.ca>
2025-01-24 16:05:08 -0500

    Just add some spacing to the none module so that when you copy from it to create a new module for a platform you don't have to add spaces.

--- Modules/Storage/None/StorageModule.cpp
+++ Modules/Storage/None/StorageModule.cpp
@@ -3,28 +3,36 @@
 
 ErrorType Storage::initStorage() {
     return ErrorType::NotImplemented;
-} 
+}
+
 ErrorType Storage::deinitStorage() {
     return ErrorType::NotImplemented;
-} 
+}
+
 ErrorType Storage::maxStorageSize(Bytes &size, std::string partitionName) {
     return ErrorType::NotImplemented;
 }
+
 ErrorType Storage::availableStorage(Bytes &size, std::string partitionName) {
     return ErrorType::NotImplemented;
 }
+
 ErrorType Storage::maxRamSize(Bytes &size, std::string memoryRegionName) {
     return ErrorType::NotImplemented;
 }
+
 ErrorType Storage::availableRam(Bytes &size, std::string memoryRegionName) {
     return ErrorType::NotImplemented;
 }
+
 ErrorType Storage::erasePartition(const std::string &partitionName) {
     return ErrorType::NotImplemented;
 }
+
 ErrorType Storage::eraseAllPartitions() {
     return ErrorType::NotImplemented;
 }
+
 ErrorType Storage::mainLoop() {
     return ErrorType::NotImplemented;
 }
