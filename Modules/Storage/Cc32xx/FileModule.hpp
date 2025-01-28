9a0d6c5ed4ef64d4fb5407077a07b78062e59ab1
Ben Haubrich <bjh885@mail.usask.ca>
2025-01-28 00:07:00 -0500

    Implement file module for Cc32xx. It appears that access to the file system is not allowed while the device is provisioning probably because it is blocking the sl_Task. It is not entirely tested but is not regressing Apf from before it was implemented so will commit what I have for now.

--- Modules/Storage/Cc32xx/FileModule.hpp
+++ Modules/Storage/Cc32xx/FileModule.hpp
@@ -3,6 +3,8 @@
 
 //AbstractionLayer
 #include "FileAbstraction.hpp"
+//TI driverlib
+#include "ti/drivers/net/wifi/fs.h"
 
 class File : public FileAbstraction {
 
@@ -22,6 +24,34 @@ class File : public FileAbstraction {
     ErrorType writeNonBlocking(const std::shared_ptr<std::string> data, std::function<void(const ErrorType error, const Bytes bytesWritten)> callback = nullptr) override;
     ErrorType synchronize() override;
     std::string path() const override;
+
+    private:
+    _i32 _deviceFileHandle = -1;
+
+    _u32 toCc32xxAccessMode(const OpenMode mode, ErrorType &error) {
+        error = ErrorType::Success;
+        switch (mode) {
+            case OpenMode::ReadOnly:
+                return SL_FS_READ;
+            case OpenMode::ReadWriteAppend:
+                return SL_FS_READ | SL_FS_WRITE;
+            case OpenMode::ReadWriteAppendNew:
+                return SL_FS_READ | SL_FS_WRITE | SL_FS_CREATE;
+            case OpenMode::ReadWriteTruncate:
+                return SL_FS_READ | SL_FS_WRITE | SL_FS_OVERWRITE;
+            case OpenMode::ReadWriteTruncateNew:
+                return SL_FS_READ | SL_FS_WRITE | SL_FS_CREATE | SL_FS_OVERWRITE;
+            case OpenMode::WriteOnlyAppend:
+                return SL_FS_WRITE;
+            case OpenMode::WriteOnlyAppendNew:
+                return SL_FS_WRITE | SL_FS_CREATE;
+            case OpenMode::WriteOnlyTruncate:
+                return SL_FS_WRITE | SL_FS_OVERWRITE;
+            default:
+                error = ErrorType::NotAvailable;
+                return SL_FS_READ;
+        }
+    }
 };
 
 #endif //__FILE_MODULE_HPP__
