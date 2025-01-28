417b9d9299d06e0fbdf5340f0b79c5ca8f6d58a2
Ben Haubrich <bjh885@mail.usask.ca>
2025-01-28 00:05:05 -0500

    Add more errors

--- Modules/Error/Cc32xx/ErrorModule.cpp
+++ Modules/Error/Cc32xx/ErrorModule.cpp
@@ -8,10 +8,12 @@
 #include "ti/drivers/net/wifi/errors.h"
 #include "errno.h"
 
+//TI does not document all their error values very well. A good trick to find more information is to grep it in the SDK docs
+//and see where it appears in there and then go look at those pages to see if there is any more information.
 ErrorType fromPlatformError(int32_t err) {
 
     //A switch statement would be nice but there are duplicate error values.
-    if (err == Power_SOK || err == PWM_STATUS_SUCCESS) {
+    if (err == Power_SOK || err == PWM_STATUS_SUCCESS || err == SL_FS_OK) {
         return ErrorType::Success;
     }
     else if (err == Power_EFAIL || err == PWM_STATUS_ERROR) {
@@ -20,7 +22,7 @@ ErrorType fromPlatformError(int32_t err) {
     else if (err == Power_EINVALIDINPUT || err == Power_EINVALIDPOINTER || err == PWM_STATUS_UNDEFINEDCMD || err == PWM_STATUS_INVALID_DUTY) {
         return ErrorType::InvalidParameter;
     }
-    else if (err == Power_ECHANGE_NOT_ALLOWED || err == Power_EBUSY) {
+    else if (err == Power_ECHANGE_NOT_ALLOWED || err == Power_EBUSY || SL_RET_CODE_PROVISIONING_IN_PROGRESS) {
         return ErrorType::PrerequisitesNotMet;
     }
     else if (SL_ERROR_ROLE_STA_ERR || SL_ERROR_ROLE_AP_ERR || SL_ERROR_ROLE_P2P_ERR || SL_ERROR_CALIB_FAIL || SL_ERROR_FS_CORRUPTED_ERR || SL_ERROR_FS_ALERT_ERR ||
@@ -35,6 +37,12 @@ ErrorType fromPlatformError(int32_t err) {
     }
     else if (err == EINVAL) {
         return ErrorType::InvalidParameter;
+    }
+    else if (err == SL_ERROR_FS_FILE_ALREADY_EXISTS) {
+        return ErrorType::FileExists;
+    }
+    else if (err == SL_ERROR_FS_FILE_NOT_EXISTS) {
+        return ErrorType::FileNotFound;
     }
     else {
         PLT_LOGW("ErrnoError", "Got unhandled error code %d", err);
