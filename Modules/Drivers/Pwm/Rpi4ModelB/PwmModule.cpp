a18128ed9e3724c193d970da12f2da44b9180953
Ben Haubrich <bjh885@mail.usask.ca>
2025-03-11 10:55:41 -0400

    Add Submodule for rpi4 model B pwm

---
+++ Modules/Drivers/Pwm/Rpi4ModelB/PwmModule.cpp
@@ -0,0 +1,29 @@
+#include "PwmModule.hpp"
+
+ErrorType Pwm::init() {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::setHardwareConfig(PeripheralNumber peripheral) {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::setDriverConfig(Percent duty, Milliseconds period) {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::setFirmwareConfig() {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::deinit() {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::start() {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::stop() {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::setDutyCycle(Percent on) {
+    return ErrorType::NotImplemented;
+}
+ErrorType Pwm::setPeriod(Milliseconds frequency) {
+    return ErrorType::NotImplemented;
+}
---
+++ Modules/Drivers/Pwm/Rpi4ModelB/PwmModule.hpp
@@ -0,0 +1,22 @@
+#ifndef __PWM_MODULE_HPP__
+#define __PWM_MODULE_HPP__
+
+#include "PwmAbstraction.hpp"
+
+class Pwm : public PwmAbstraction {
+    public:
+    Pwm() : PwmAbstraction() {}
+    ~Pwm() = default;
+
+    ErrorType init() override;
+    ErrorType setHardwareConfig(PeripheralNumber peripheral) override;
+    ErrorType setDriverConfig(Percent duty, Milliseconds period) override;
+    ErrorType setFirmwareConfig() override;
+    ErrorType deinit() override;
+    ErrorType start() override;
+    ErrorType stop() override;
+    ErrorType setDutyCycle(Percent on) override;
+    ErrorType setPeriod(Milliseconds period) override;
+};
+
+#endif // __PWM_MODULE_HPP__
