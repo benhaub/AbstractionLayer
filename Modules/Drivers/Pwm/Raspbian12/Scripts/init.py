c7aa451b65ac45387de7222004e604bdd1c7bba3
Ben Haubrich <bjh885@mail.usask.ca>
2025-03-13 15:07:42 -0400

    Add support for the pin paremeter.

--- Modules/Drivers/Pwm/Raspbian12/Scripts/init.py
+++ Modules/Drivers/Pwm/Raspbian12/Scripts/init.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     parser.add_argument('channel', type=int, choices=[0, 1],
                             help="PWM channel number."
                        )
-    parser.add_argument('outputPin', type=int, choices=[12, 18],
+    parser.add_argument('outputPin', type=int, choices=[12, 18, 13, 19],
                             help="PWM output pin number."
                        )
     parser.add_argument('period', type=int,
@@ -41,11 +41,20 @@ if __name__ == '__main__':
     #Only zero is allowed by this script's argparse, but leaving this here in case using pwm1 is implemented in the future.
     #`dtoverlay pwm1` doesn't seem to activate the overlay on Raspbian 12, leaving only PWM0 with either channel 0 or 1 to use.
     pwmPeripheral = 'pwm' if 0 == args.peripheral else 'pwm' + str(args.peripheral)
+    function = ''
+    if (12 == args.outputPin):
+        function = 4
+    elif (18 == args.outputPin):
+        function = 2
+    elif (13 == args.outputPin):
+        function = 4
+    elif (19 == args.outputPin):
+        function = 2
 
     #https://raspberrypi.stackexchange.com/questions/143643/how-can-i-use-dtoverlay-pwm/143644#143644
     #https://www.kernel.org/doc/html/v5.10/driver-api/pwm.html#using-pwms-with-the-sysfs-interface
     dtOverlayCommand = ['dtoverlay',
-                        pwmPeripheral]
+                        pwmPeripheral + ',' + 'pin=' + str(args.outputPin) + ' func=' + str(function)]
     subprocess.run(dtOverlayCommand)
 
     pwmOverlayExport = Path('/sys/class/pwm/pwmchip0/export')
