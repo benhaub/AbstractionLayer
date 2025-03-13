671ca9957b979fed70749eb85bd5fc401a1ed616
Ben Haubrich <bjh885@mail.usask.ca>
2025-03-12 22:42:57 -0400

    Edit init and deinit scripts for raspbian 12 to restrict the peripheral number to zero and add channel arguments.

--- Modules/Drivers/Pwm/Raspbian12/Scripts/deinit.py
+++ Modules/Drivers/Pwm/Raspbian12/Scripts/deinit.py
@@ -20,8 +20,11 @@ if __name__ == '__main__':
                                      epilog="Created by Ben Haubrich March 12th, 2025")
 
     #By not providing a default value these are made required.
-    parser.add_argument('peripheral', type=int, choices=[0, 1],
+    parser.add_argument('peripheral', type=int, choices=[0],
                             help="PWM peripheral number."
+                       )
+    parser.add_argument('channel', type=int, choices=[0, 1],
+                            help="PWM channel number."
                        )
 
     args = parser.parse_args()
@@ -29,25 +32,27 @@ if __name__ == '__main__':
     #Uncomment for help with debugging.
     #print("{}".format(args))
 
-    pwmOverlay = 'pwm' if 0 == args.peripheral else 'pwm' + str(args.peripheral)
+    #Only zero is allowed by this script's argparse, but leaving this here in case using pwm1 is implemented in the future.
+    #`dtoverlay pwm1` doesn't seem to activate the overlay on Raspbian 12, leaving only PWM0 with either channel 0 or 1 to use.
+    pwmPeripheral = 'pwm' if 0 == args.peripheral else 'pwm' + str(args.peripheral)
 
     #https://raspberrypi.stackexchange.com/questions/143643/how-can-i-use-dtoverlay-pwm/143644#143644
     #https://www.kernel.org/doc/html/v5.10/driver-api/pwm.html#using-pwms-with-the-sysfs-interface
     pwmOverlayUnexport = Path('/sys/class/pwm/pwmchip0/unexport')
-    pwmOverlayEnable = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.peripheral) + '/enable')
+    pwmOverlayEnable = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.channel) + '/enable')
 
     try:
         with pwmOverlayEnable.open('w') as enable:
             enable.write('0')
 
         with pwmOverlayUnexport.open('w') as unexport:
-            unexport.write(str(args.peripheral))
+            unexport.write(str(args.channel))
     except FileNotFoundError:
         pass
 
     cmakeCommand = ['dtoverlay',
                     '-r',
-                    pwmOverlay]
+                    pwmPeripheral]
     subprocess.run(cmakeCommand)
 
     exit(0)
--- Modules/Drivers/Pwm/Raspbian12/Scripts/init.py
+++ Modules/Drivers/Pwm/Raspbian12/Scripts/init.py
@@ -14,8 +14,14 @@ if __name__ == '__main__':
                                      epilog="Created by Ben Haubrich March 11th, 2025")
 
     #By not providing a default value these are made required.
-    parser.add_argument('peripheral', type=int, choices=[0, 1],
+    parser.add_argument('peripheral', type=int, choices=[0],
                             help="PWM peripheral number."
+                       )
+    parser.add_argument('channel', type=int, choices=[0, 1],
+                            help="PWM channel number."
+                       )
+    parser.add_argument('outputPin', type=int,
+                            help="PWM output pin number."
                        )
     parser.add_argument('period', type=int,
                             help="PWM period in nanoseconds."
@@ -29,23 +35,24 @@ if __name__ == '__main__':
     #Uncomment for help with debugging.
     #print("{}".format(args))
 
-    pwmOverlay = 'pwm' if 0 == args.peripheral else 'pwm' + str(args.peripheral)
-    print(pwmOverlay)
+    #Only zero is allowed by this script's argparse, but leaving this here in case using pwm1 is implemented in the future.
+    #`dtoverlay pwm1` doesn't seem to activate the overlay on Raspbian 12, leaving only PWM0 with either channel 0 or 1 to use.
+    pwmPeripheral = 'pwm' if 0 == args.peripheral else 'pwm' + str(args.peripheral)
 
     #https://raspberrypi.stackexchange.com/questions/143643/how-can-i-use-dtoverlay-pwm/143644#143644
     #https://www.kernel.org/doc/html/v5.10/driver-api/pwm.html#using-pwms-with-the-sysfs-interface
-    cmakeCommand = ['dtoverlay',
-                    pwmOverlay]
-    subprocess.run(cmakeCommand)
+    dtOverlayCommand = ['dtoverlay',
+                        pwmPeripheral]
+    subprocess.run(dtOverlayCommand)
 
     pwmOverlayExport = Path('/sys/class/pwm/pwmchip0/export')
-    pwmOverlayPeriod = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.peripheral) + '/period')
-    pwmOverlayDuty = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.peripheral) + '/duty_cycle')
-    pwmOverlayEnable = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.peripheral) + '/enable')
+    pwmOverlayPeriod = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.channel) + '/period')
+    pwmOverlayDuty = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.channel) + '/duty_cycle')
+    pwmOverlayEnable = Path('/sys/class/pwm/pwmchip0/' + 'pwm' + str(args.channel) + '/enable')
 
     #None of the below files will be present until we export.
     with pwmOverlayExport.open('w') as export:
-        export.write(str(args.peripheral))
+        export.write(str(args.outputPin))
 
     with pwmOverlayPeriod.open('w') as setPeriod:
         setPeriod.write(str(args.period))
