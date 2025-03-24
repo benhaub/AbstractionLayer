fd7095ce2086a46a5aecd876431ace8532f6fc51
Ben Haubrich <bjh885@mail.usask.ca>
2025-03-24 00:56:52 -0400

    Allow more than 2 PWM counters to be created by managing which groups the timers are initialized with.

--- Modules/Drivers/Gptm/Esp/GptmPwmModule.cpp
+++ Modules/Drivers/Gptm/Esp/GptmPwmModule.cpp
@@ -4,11 +4,26 @@
 #include "driver/mcpwm_oper.h"
 #include "driver/mcpwm_gen.h"
 
+namespace {
+    Count _GroupZeroActiveTimers = 0;
+    Count _GroupOneActiveTimers = 0;
+}
+
 ErrorType GptmPwmModule::init() {
     assert(PeripheralNumber::Unknown != peripheralNumber());
 
+    if (_GroupZeroActiveTimers < _MaxTimersPerGroup) {
+        _groupId = 0;
+    }
+    else if (_GroupOneActiveTimers < _MaxTimersPerGroup) {
+        _groupId = 1;
+    }
+    else {
+        return ErrorType::NotAvailable;
+    }
+
     const mcpwm_timer_config_t timerConfig = {
-        .group_id = 0,
+        .group_id = _groupId,
         .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
         .resolution_hz = 1000000,
         .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
@@ -17,29 +32,33 @@ ErrorType GptmPwmModule::init() {
 
     esp_err_t err;
     if (ESP_OK == (err = mcpwm_new_timer(&timerConfig, &_timer))) {
-        mcpwm_oper_handle_t oper = NULL;
         const mcpwm_operator_config_t operatorConfig = {
-            .group_id = 0
+            .group_id = _groupId
         };
-        if (ESP_OK == (err = mcpwm_new_operator(&operatorConfig, &oper))) {
-            if (ESP_OK == (err = mcpwm_operator_connect_timer(oper, _timer))) {
+        if (ESP_OK == (err = mcpwm_new_operator(&operatorConfig, &_operator))) {
+            if (ESP_OK == (err = mcpwm_operator_connect_timer(_operator, _timer))) {
                 const mcpwm_comparator_config_t comparatorConfig = {
                     .flags = {
                         .update_cmp_on_tez = true
                     }
                 };
-                if (ESP_OK == (err = mcpwm_new_comparator(oper, &comparatorConfig, &_comparator))) {
-                    mcpwm_gen_handle_t generator = NULL;
+                if (ESP_OK == (err = mcpwm_new_comparator(_operator, &comparatorConfig, &_comparator))) {
                     const mcpwm_generator_config_t generatorConfig = {
                         .gen_gpio_num = outputPinConst()
                     };
-                    if (ESP_OK == (err = mcpwm_new_generator(oper, &generatorConfig, &generator))) {
+                    if (ESP_OK == (err = mcpwm_new_generator(_operator, &generatorConfig, &_generator))) {
                         setDutyCycle(_dutyCycle);
                         //Compare event to cause the signal to go high
-                        mcpwm_generator_set_action_on_timer_event(generator, MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
+                        mcpwm_generator_set_action_on_timer_event(_generator, MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH));
                         //Compare event to cause the signal to go low
-                        mcpwm_generator_set_action_on_compare_event(generator, MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, _comparator, MCPWM_GEN_ACTION_LOW));
+                        mcpwm_generator_set_action_on_compare_event(_generator, MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, _comparator, MCPWM_GEN_ACTION_LOW));
                         if (ESP_OK == (err = mcpwm_timer_enable(_timer))) {
+                            if (0 == _groupId) {
+                                _GroupZeroActiveTimers += 1;
+                            }
+                            else if (1 == _groupId) {
+                                _GroupOneActiveTimers += 1;
+                            }
                             return ErrorType::Success;
                         }
                     }
@@ -52,7 +71,23 @@ ErrorType GptmPwmModule::init() {
 }
 
 ErrorType GptmPwmModule::deinit() {
-    return ErrorType::NotImplemented;
+    assert(_groupId != 1);
+    assert(_GroupZeroActiveTimers >= 0);
+    assert(_GroupOneActiveTimers >= 0);
+
+    if (0 == _groupId) {
+        _GroupZeroActiveTimers -= 1;
+    }
+    else if (1 == _groupId) {
+        _GroupOneActiveTimers -= 1;
+    }
+
+    mcpwm_del_timer(_timer);
+    mcpwm_del_comparator(_comparator);
+    mcpwm_del_generator(_generator);
+    mcpwm_del_operator(_operator);
+
+    return ErrorType::Success;
 }
 
 ErrorType GptmPwmModule::start() {
--- Modules/Drivers/Gptm/Esp/GptmPwmModule.hpp
+++ Modules/Drivers/Gptm/Esp/GptmPwmModule.hpp
@@ -5,12 +5,16 @@
 //ESP
 #include "driver/mcpwm_timer.h"
 #include "driver/mcpwm_cmpr.h"
+#include "driver/mcpwm_gen.h"
+#include "driver/mcpwm_oper.h"
 
 class GptmPwmModule : public GptmPwmAbstraction {
 
     public:
     GptmPwmModule() : GptmPwmAbstraction() {}
     ~GptmPwmModule() = default;
+
+    static constexpr Count _MaxTimersPerGroup = 2;
 
     ErrorType init() override;
     ErrorType deinit() override;
@@ -20,8 +24,11 @@ class GptmPwmModule : public GptmPwmAbstraction {
     ErrorType setPeriod(const Microseconds period) override;
 
     private:
+    int _groupId = -1;
     mcpwm_timer_handle_t _timer = nullptr;
     mcpwm_cmpr_handle_t _comparator = nullptr;
+    mcpwm_gen_handle_t _generator = nullptr;
+    mcpwm_oper_handle_t _operator = nullptr;
 
     uint32_t microsecondsToEspTimerTicks(const Microseconds period) {
         return (period);
