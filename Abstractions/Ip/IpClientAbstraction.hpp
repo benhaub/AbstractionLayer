a42998e1c296231efb71058bd94671856b36a0f8
Ben Haubrich <bjh885@mail.usask.ca>
2025-07-29 20:37:07 -0400

    Rename client types namespace.

--- Abstractions/Ip/IpClientAbstraction.hpp
+++ Abstractions/Ip/IpClientAbstraction.hpp
@@ -16,13 +16,13 @@
  * @namespace IpTypes
  * @brief Types for the IP client
 */
-namespace IpTypes {
+namespace IpClientTypes {
 
     /**
      * @struct ClientStatus
      * @brief The status of the client
     */
-    struct ClientStatus {
+    struct Status {
         bool connected; ///< True when the client is connected to the host.
     };
 }
@@ -191,7 +191,7 @@ class IpClientAbstraction {
     /// @brief Get the network abstraction that this client communicates on as a constant reference
     const NetworkAbstraction &networkConst() const { assert(nullptr != _network); return *_network; }
     /// @brief Get the status of the client as a const reference
-    const IpTypes::ClientStatus &statusConst() const { return _status; }
+    const IpClientTypes::Status &statusConst() const { return _status; }
 
     /// @brief Set the network abstraction that this client communicates on.
     /// @param[in] network The network abstraction to use.
@@ -209,7 +209,7 @@ class IpClientAbstraction {
     /// @brief The port
     Port _port = 0;
     /// @brief The status of the client
-    IpTypes::ClientStatus _status = {
+    IpClientTypes::Status _status = {
         false
     };
 
