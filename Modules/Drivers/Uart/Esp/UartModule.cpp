a2d45eb067492b87d86b834b65dcab222a8568f5
Ben Haubrich <bjh885@mail.usask.ca>
2025-08-27 14:54:08 -0400

    Re-arrange the order in which uart is initialized to match ESPs example code

--- Modules/Drivers/Uart/Esp/UartModule.cpp
+++ Modules/Drivers/Uart/Esp/UartModule.cpp
@@ -2,34 +2,32 @@
 #include "UartModule.hpp"
 //ESP
 #include "hal/uart_hal.h"
-//C++
-#include <string>
 
 ErrorType Uart::init() {
     ErrorType error;
+    const uart_port_t uartPort = toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error);
 
-    uart_port_t uartPort = toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error);
     if (ErrorType::Success == error) {
-        if (ESP_OK == uart_set_pin(uartPort, uartParams().hardwareConfig.tx, uartParams().hardwareConfig.rx, uartParams().hardwareConfig.rts, uartParams().hardwareConfig.cts)) {
+
+        if (ESP_OK == uart_driver_install(uartPort, uartParams().firmwareConfig.receiveBufferSize, uartParams().firmwareConfig.transmitBufferSize, 0, nullptr, 0)) {
             uart_config_t uartConfig;
             uartConfig.baud_rate = uartParams().driverConfig.baudRate;
             uartConfig.rx_flow_ctrl_thresh = 0;
             uartConfig.source_clk = UART_SCLK_DEFAULT;
-
             uartConfig.data_bits = toEspWordLength(uartParams().driverConfig.dataBits, error);
+
+            uartConfig.parity = toEspUartParity(uartParams().driverConfig.parity, error);
+
             if (ErrorType::Success == error) {
-                uartConfig.parity = toEspUartParity(uartParams().driverConfig.parity, error);
+                uartConfig.stop_bits = toEspStopBits(uartParams().driverConfig.stopBits, error);
 
                 if (ErrorType::Success == error) {
-                    uartConfig.stop_bits = toEspStopBits(uartParams().driverConfig.stopBits, error);
+                    uartConfig.flow_ctrl = toEspFlowControl(uartParams().driverConfig.flowControl, error);
 
-                    if (ErrorType::Success == error) {
-                        uartConfig.flow_ctrl = toEspFlowControl(uartParams().driverConfig.flowControl, error);
+                    if (ESP_OK == uart_param_config(uartPort, &uartConfig)) {
 
-                        if (ErrorType::Success == error) {
-                            if (ESP_OK == uart_param_config(toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error), &uartConfig)) {
-                                error = fromPlatformError(uart_driver_install(toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error), uartParams().firmwareConfig.receiveBufferSize, uartParams().firmwareConfig.transmitBufferSize, 0, nullptr, 0));
-                            }
+                        if (ESP_OK == uart_set_pin(uartPort, uartParams().hardwareConfig.tx, uartParams().hardwareConfig.rx, uartParams().hardwareConfig.rts, uartParams().hardwareConfig.cts)) {
+                            error = ErrorType::Success;
                         }
                     }
                 }
@@ -41,40 +39,45 @@ ErrorType Uart::init() {
 }
 
 ErrorType Uart::deinit() {
-    ErrorType error;
-    return fromPlatformError(uart_driver_delete(toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error)));
+    ErrorType error = ErrorType::Failure;
+    const uart_port_t uartPort = toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error);
+
+    if (ErrorType::Success == error) {
+        error = fromPlatformError(uart_driver_delete(uartPort));
+    }
+
+    return error;
 }
 
 ErrorType Uart::txBlocking(const std::string &data, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params) {
-    ErrorType error;
-    const PeripheralNumber peripheralNumber = uartParams().hardwareConfig.peripheralNumber;
+    ErrorType error = ErrorType::Failure;
+    const uart_port_t uartPort = toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error);
 
-    if (-1 != uart_write_bytes(toEspPeripheralNumber(peripheralNumber, error), data.data(), data.size())) {
-        return ErrorType::Success;
+    if (ErrorType::Success == error) {
+        if (-1 != uart_write_bytes(uartPort, data.data(), data.size())) {
+            error = ErrorType::Success;
+        }
     }
 
-    return ErrorType::Failure;
+    return error;
 }
 
 ErrorType Uart::rxBlocking(std::string &buffer, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params) {
-    ErrorType error;
-    esp_err_t result;
-    constexpr Bytes toReadAtOnce = 16;
-    std::string readBuffer(toReadAtOnce, '\0');
-    const PeripheralNumber peripheralNumber = uartParams().hardwareConfig.peripheralNumber;
+    ErrorType error = ErrorType::Failure;
+    const uart_port_t uartPort = toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error);
 
-    uart_port_t uartNumber = toEspPeripheralNumber(peripheralNumber, error);
-    if (ErrorType::Success != error) {
-        return error;
+    if (ErrorType::Success == error) {
+        const esp_err_t result = uart_read_bytes(uartPort, &buffer[0], buffer.size(), pdMS_TO_TICKS(timeout));
+        if (result > 0) {
+            buffer.resize(result);
+            error = ErrorType::Success;
+        }
+        else if (0 == result) {
+            error = ErrorType::Timeout;
+        }
     }
 
-    result = uart_read_bytes(uartNumber, buffer.data(), buffer.size(), pdMS_TO_TICKS(timeout));
-    if (result > 0) {
-        buffer.resize(result);
-        return ErrorType::Success;
-    }
-
-    return ErrorType::Timeout;
+    return error;
 }
 
 ErrorType Uart::txNonBlocking(const std::shared_ptr<std::string> data, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params, std::function<void(const ErrorType error, const Bytes bytesWritten)> callback) {
@@ -82,11 +85,11 @@ ErrorType Uart::txNonBlocking(const std::shared_ptr<std::string> data, const Mil
 }
 
 ErrorType Uart::rxNonBlocking(std::shared_ptr<std::string> buffer, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params, std::function<void(const ErrorType error, std::shared_ptr<std::string> buffer)> callback) {
-    auto rx = [&, callback](std::shared_ptr<std::string> buffer, Bytes size) -> ErrorType {
+    auto rx = [&, callback, buffer]() -> ErrorType {
         ErrorType error = ErrorType::Failure;
         const Milliseconds timeout = 1000;
 
-        error = rxBlocking(*(buffer.get()), timeout, params);
+        error = rxBlocking(*buffer, timeout, params);
 
         if (nullptr != callback) {
             callback(error, buffer);
@@ -95,20 +98,17 @@ ErrorType Uart::rxNonBlocking(std::shared_ptr<std::string> buffer, const Millise
         return error;
     };
 
-    EventQueue::Event event = EventQueue::Event(std::bind(rx, buffer, buffer->size()));
+    EventQueue::Event event = EventQueue::Event(rx);
     return addEvent(event);
 }
 
 ErrorType Uart::flushRxBuffer() {
-    ErrorType error;
-    const PeripheralNumber peripheralNumber = uartParams().hardwareConfig.peripheralNumber;
+    ErrorType error = ErrorType::Failure;
+    const uart_port_t uartPort = toEspPeripheralNumber(uartParams().hardwareConfig.peripheralNumber, error);
 
-    esp_err_t err = uart_flush_input(toEspPeripheralNumber(peripheralNumber, error));
+    if (ErrorType::Success == error) {
+        error = fromPlatformError(uart_flush_input(uartPort));
+    }
 
-    if (ErrorType::Success != error) {
-        return error;
-    }
-    else {
-        return fromPlatformError(err);
-    }
+    return error;
 }
