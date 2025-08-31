f9e4872f1709c3ba0abb413309f475e8b7bf83a9
Ben Haubrich <bjh885@mail.usask.ca>
2025-08-30 21:43:39 -0400

    Add missing function defintions

--- Modules/Drivers/I2c/Esp/I2cModule.cpp
+++ Modules/Drivers/I2c/Esp/I2cModule.cpp
@@ -116,6 +116,9 @@ ErrorType I2c::txBlocking(const StaticString::Container &data, const Millisecond
     //return error;
     return ErrorType::NotImplemented;
 }
+ErrorType I2c::txBlocking(const std::string &data, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params) {
+    return ErrorType::NotImplemented;
+}
 
 ErrorType I2c::rxBlocking(StaticString::Container &buffer, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params) {
     //assert(std::nullopt != params.i2cDeviceAddress);
@@ -147,13 +150,22 @@ ErrorType I2c::rxBlocking(StaticString::Container &buffer, const Milliseconds ti
 //    return error;
     return ErrorType::NotImplemented;
 }
+ErrorType I2c::rxBlocking(std::string &buffer, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params) {
+    return ErrorType::NotImplemented;
+}
 
 ErrorType I2c::txNonBlocking(const std::shared_ptr<StaticString::Container> data, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params, std::function<void(const ErrorType error, const Bytes bytesWritten)> callback) {
     return ErrorType::NotImplemented;
 }
+ErrorType I2c::txNonBlocking(const std::shared_ptr<std::string> data, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params, std::function<void(const ErrorType error, const Bytes bytesWritten)> callback) {
+    return ErrorType::NotAvailable;
+}
 
 ErrorType I2c::rxNonBlocking(std::shared_ptr<StaticString::Container> buffer, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params, std::function<void(const ErrorType error, std::shared_ptr<StaticString::Container> buffer)> callback) {
     return ErrorType::NotImplemented;
+}
+ErrorType I2c::rxNonBlocking(std::shared_ptr<std::string> buffer, const Milliseconds timeout, const IcCommunicationProtocolTypes::AdditionalCommunicationParameters &params, std::function<void(const ErrorType error, std::shared_ptr<std::string> buffer)> callback) {
+    return ErrorType::NotAvailable;
 }
 
 ErrorType I2c::flushRxBuffer() {
