400914d6156229c77f738d1b55a0b9959314673b
Ben Haubrich <bjh885@mail.usask.ca>
2025-09-19 11:15:08 -0400

    Optionally update the status when calling it. Defaults to true

--- Abstractions/Storage/FileSystemAbstraction.hpp
+++ Abstractions/Storage/FileSystemAbstraction.hpp
@@ -245,21 +245,23 @@ class FileSystemAbstraction {
     /// @brief Get the mount prefix as a constant reference
     std::string_view mountPrefix() const { return _mountPrefix->c_str(); }
     /// @brief Get the status of the file system as a constant reference
-    const FileSystemTypes::Status &status() {
+    const FileSystemTypes::Status &status(bool updateStatus = false) {
         //Bytes is a uint32_t so on systems with more than 4GiB of storage, this will overflow.
         //which is why you might see percentages greater than 100%. Bytes is used a lot in the abstraction layer
         //and since the primary goal of it is to develop applications for embedded systems it's not going to be that
         //useful to change the type of Bytes to uint64_t and then force the CPU to do 2 accesses to memory
         //to get values.
-        Bytes available;
-        availablePartition(available);
-        Bytes maxSize;
-        maxPartitionSize(maxSize);
-        if (maxSize == 0) {
-            _status.freeSpace = 0;
-        }
-        else {
-            _status.freeSpace = ((float)available / maxSize) * 100.0f;
+        if (updateStatus) {
+            Bytes available;
+            availablePartition(available);
+            Bytes maxSize;
+            maxPartitionSize(maxSize);
+            if (maxSize == 0) {
+                _status.freeSpace = 0;
+            }
+            else {
+                _status.freeSpace = ((float)available / maxSize) * 100.0f;
+            }
         }
 
         return _status;
