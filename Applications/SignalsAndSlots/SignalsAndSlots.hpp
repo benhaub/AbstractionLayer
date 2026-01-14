0093c3fefdffe55e5fa8c173d3c4b1e9df92f9c9
Ben Haubrich <39037440+benhaub@users.noreply.github.com>
2026-01-14 18:20:46 +0000

    Fix bug with disconnect. binded functions can not look up their targets without know the exact type of the function. Even the though function demands you return an ErrorType and the argument types are known when you create the class, it appears to not be enough information.

--- Applications/SignalsAndSlots/SignalsAndSlots.hpp
+++ Applications/SignalsAndSlots/SignalsAndSlots.hpp
@@ -1,7 +1,7 @@
 /**************************************************************************//**
 * @author Ben Haubrich                                        
 * @file   SignalsAndSlots.hpp
-* @details \b Synopsis: \n Allows you to connect to emitted signals which call a callback.
+* @details Allows you to connect to signals which call a callback when emitted.
 * @see https://doc.qt.io/qt-5/signalsandslots.html
 * @see https://en.wikipedia.org/wiki/Observer_pattern
 * @ingroup Applications
@@ -13,7 +13,6 @@
 #include "EventQueue.hpp"
 //C++
 #include <atomic>
-#include <cassert>
 
 /**
  * @namespace SignalsAndSlots
@@ -41,40 +40,39 @@ namespace SignalsAndSlots {
     template <typename ...Args> class Signal {
 
         public:
-
-        Signal() {
-            _slots.fill(std::make_pair(nullptr, nullptr));
-        }
         /**
          * @brief Observe a signal and call the callback when it is emitted
          * @details Interrupt and thread safe.
-         * @param callback The observers callback
-         * @param eventQueue The event queue to add the callback to
+         * @param[in] callback The observers callback
+         * @param[in] eventQueue The event queue to add the callback to
+         * @param[out] handle The handle to the connection which can be used later to disconnect
          * @sa emit
          * @returns ErrorType::Success if the connection was successful
          * @returns ErrorType::LimitReached if the maximum number of observers has been reached
          * @returns ErrorType::Timeout if the semaphore could not be obtained in time
          * @returns ErrorType::Failure otherwise
          */
-        ErrorType connect(EventQueue &eventQueue, std::function<ErrorType(Args...)> callback) {
-            for (auto &slot : _slots) {
-                if (slot.first == nullptr) {
-                    //The only other thing that can happen to _currentNumberOfObservers in the time after we load it is that it could be decremented by
-                    //disconnect() so if we pass this guard we will not overflow the maximum allowed number of observers.
-                    Count currentNumberOfObservers = _currentNumberOfObservers.load();
-                    assert(currentNumberOfObservers <= _MaxNumberOfObservers);
+        ErrorType connect(EventQueue &eventQueue, std::function<ErrorType(Args...)> callback, Id &handle) {
+            ErrorType error = ErrorType::LimitReached;
 
-                    if (_currentNumberOfObservers.fetch_add(1, std::memory_order_relaxed)) {
-                        slot = std::make_pair(&eventQueue, callback);
-                        return ErrorType::Success;
-                    }
-                    else {
-                        continue;
-                    }
+            auto itr = find_if(_slots.begin(), _slots.end(), [](const auto &slot) {
+                return slot.first == nullptr;
+            });
+
+            if (itr != _slots.end()) {
+                //The only other thing that can happen to _currentNumberOfObservers in the time after we load it is that it could be decremented by
+                //disconnect() so if we pass this guard we will not overflow the maximum allowed number of observers.
+                if (_currentNumberOfObservers.fetch_add(1, std::memory_order_relaxed) < _MaxNumberOfObservers) {
+                    *itr = std::make_pair(&eventQueue, callback);
+                    handle = std::distance(_slots.begin(), itr);
+                    error = ErrorType::Success;
+                }
+                else {
+                    _currentNumberOfObservers.fetch_sub(1, std::memory_order_relaxed);
                 }
             }
 
-            return ErrorType::LimitReached;
+            return error;
         }
 
         /**
@@ -98,26 +96,23 @@ namespace SignalsAndSlots {
         /**
          * @brief disconnect a callback from this signal
          * @details Interrupt and thread safe.
-         * @param callback The observers callback to disconnect
-         * @returns ErrorType::Success if the disconnection was successful
+         * @param handle The handle returned from the connect() which will disconnect the slot from further signal emissions.
+         * @returns ErrorType::Success if the disconnection was successful or the handle was not found (already disconnected)
          * @returns ErrorType::Timeout if the semaphore could not be obtained in time
          * @returns ErrorType::Failure otherwise
          */
-        ErrorType disconnect(std::function<ErrorType(Args...)> callback) {
-            if (nullptr != callback.target()) {
-                auto itr = find_if(_slots, [&callback](const auto &observer) {
-                    return (nullptr != observer.target() && observer.second.target() == callback.target());
-                });
+        ErrorType disconnect(const Id handle) {
+            Id i = 0;
+            auto itr = find_if(_slots.begin(), _slots.end(), [&i, handle](const auto &observer) {
+                return i++ == handle;
+            });
 
-                if (itr != _slots.end()) {
-                    const Count currentNumberOfObservers = _currentNumberOfObservers.load(std::memory_order_relaxed);
-                    assert(currentNumberOfObservers > 0);
-                    //If _currentNumberOfObservers is equal to _MaxNumberOfObservers, and we are interrupted or pre-empted before we can decrement
-                    //_currentNumberOfOberservers, then anything that tries to connect() will still see the queue as full.
-                    //If it is any other the value, then connect() will just select the next available spot.
-                    itr = std::make_pair(nullptr, nullptr);
-                    _currentNumberOfObservers.fetch_sub(1, std::memory_order_relaxed);
-                }
+            if (itr != _slots.end()) {
+                //If _currentNumberOfObservers is equal to _MaxNumberOfObservers, and we are interrupted or pre-empted before we can decrement
+                //_currentNumberOfOberservers, then anything that tries to connect() will still see the queue as full.
+                //If it is any other the value, then connect() will just select the next available spot.
+                *itr = std::make_pair(nullptr, nullptr); 
+                _currentNumberOfObservers.fetch_sub(1, std::memory_order_relaxed);
             }
             
             return ErrorType::Success;
@@ -129,7 +124,7 @@ namespace SignalsAndSlots {
         /// @brief The current number of observers in the slots array.
         std::atomic<Count> _currentNumberOfObservers = 0;
         /// @brief List of all observer callbacks
-        std::array<std::pair<EventQueue *, std::function<ErrorType(Args...)>>, _MaxNumberOfObservers> _slots;
+        std::array<std::pair<EventQueue *, std::function<ErrorType(Args...)>>, _MaxNumberOfObservers> _slots = {std::make_pair(nullptr, nullptr)};
 
         /**
          * @brief Calls all of the observers with the callbacks they have registered using the connect() call
@@ -143,15 +138,15 @@ namespace SignalsAndSlots {
         ErrorType _emit([[maybe_unused]] const std::tuple<Args...> params, [[maybe_unused]] const std::index_sequence<IndexSequence...>) const {
             ErrorType error = ErrorType::NoData;
 
-            for (const auto &slot : _slots) {
+            for (auto &slot : _slots) {
+
                 if (nullptr != slot.first && nullptr != slot.second) {
                     EventQueue &eventQueue = *(slot.first);
                     const std::function<ErrorType(Args...)> &callback = slot.second;
 
                     //The pack is IndexSequence, so the expansion is std::get<0>(params), std::get<1>(params), etc.
                     EventQueue::Event event = EventQueue::Event(std::bind(callback, std::get<IndexSequence>(params)...));
-                    ErrorType addEventError = eventQueue.addEvent(event);
-                    error = addEventError;
+                    error = eventQueue.addEvent(event);
                 }
             }
 
